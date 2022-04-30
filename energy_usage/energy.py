790de2f349ca438c4d29440c2be0452a067df8ea
rafaelsilvadp <1190979@isep.ipp.pt>
2022-04-30 15:42:48 +0100

    energy-usage

--- energy_usage/energy.py
+++ energy_usage/energy.py
@@ -27,16 +27,59 @@ def stoogesort(arr, l, h):
 		# again to confirm
 		stoogesort(arr, l, (h-t))
 
-def dummy_sorting_alg():
-    data = pd.read_csv (r'PT 042021-032022.csv')   
-    arr = data['carbon_intensity_production_avg'].tolist()
-    n = len(arr)
-    stoogesort(arr, 0, n-1)
-    print("Array has been sorted!")
+def mergeSort(arr):
+	if len(arr) > 1:
+
+		# Finding the mid of the array
+		mid = len(arr)//2
+
+		# Dividing the array elements
+		L = arr[:mid]
+
+		# into 2 halves
+		R = arr[mid:]
+
+		# Sorting the first half
+		mergeSort(L)
+
+		# Sorting the second half
+		mergeSort(R)
+
+		i = j = k = 0
+
+		# Copy data to temp arrays L[] and R[]
+		while i < len(L) and j < len(R):
+			if L[i] < R[j]:
+				arr[k] = L[i]
+				i += 1
+			else:
+				arr[k] = R[j]
+				j += 1
+			k += 1
+
+		# Checking if any element was left
+		while i < len(L):
+			arr[k] = L[i]
+			i += 1
+			k += 1
+
+		while j < len(R):
+			arr[k] = R[j]
+			j += 1
+			k += 1
+
+def dummy_sorting_alg(alg):
+	data = pd.read_csv (r'PT 042021-032022.csv')   
+	arr = data['carbon_intensity_production_avg'].tolist()
+	if alg == "stooge":
+		n = len(arr)
+		stoogesort(arr, 0, 1000)
+	else:
+		mergeSort(arr)
 
 def main():
-    energyusage.evaluate(dummy_sorting_alg, pdf=True)
+	energyusage.evaluate(dummy_sorting_alg,"stooge", pdf=True)
 
 if __name__ == "__main__":
-    main()
+	main()
 
