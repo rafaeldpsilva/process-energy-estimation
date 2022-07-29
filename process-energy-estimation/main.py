12b21848b28bbd51632cfc2063c1cbf3418b69bf
Rafael Silva <1190979@isep.ipp.pt>
2022-07-29 15:03:25 +0100

    add baseline energy consumption reading

--- process-energy-estimation/main.py
+++ process-energy-estimation/main.py
@@ -5,10 +5,27 @@ import configuration
 import cmd
 import convert
 import report
+import estimate
 import psutil
 import pandas as pd
 
 
+def measure_baseline_wattage():
+    if(True):
+        configuration.initialize_files(configuration.get_powerlog_filename(), configuration.get_process_filename(), configuration.get_nvidia_smi_filename())
+        check_seconds = configuration.get_base_check_seconds()
+        cmd.get_cpu_base_report(configuration.get_powerlog_filename(), check_seconds)
+        cmd.get_gpu_base_report(configuration.get_nvidia_smi_filename(),configuration.get_interval(),check_seconds)
+        
+    [powerlog_data,general_data] = utils.read_powerlog_file(configuration.get_powerlog_filename(),configuration.get_physical_cpu_sockets())
+    array = []
+    for i in general_data:
+        array.append(i.replace(" ", "").split("="))
+
+    [gpu_units,gpu_energy,average_gpu_power] = estimate.estimate_gpu_power_consumption(float(array[0][1]),configuration.get_nvidia_smi_filename())
+    print("Baseline Wattage\n\n========CPU=======\nProcessor Energy: {} Joules\nProcessor Energy: {} mWh\nAverage Processor Power: {} Watt".format(array[2][1],array[3][1],array[4][1]))
+    print("========DRAM======\nDRAM Energy: {} Joules\nDRAM Energy: {} mWh\nAverage DRAM Power: {} Watt".format(array[8][1],array[9][1],array[10][1]))
+    print("========GPU======\nGPU Energy: {} Joules\nGPU Energy: {} mWh\nAverage GPU Power: {} Watt\n\n".format(gpu_energy[0]*3600,gpu_energy[0],average_gpu_power[0]))
 
 def measure_process_cpu_usage(process_filename, pid):
     """Registers the cpu usage of the process with given pid on the specified file."""
@@ -39,12 +56,12 @@ def measure_process_cpu_usage(process_filename, pid):
             f.write(datetime.datetime.now().strftime("%H:%M:%S:%f") + "," + str(process_cpu_usage) + "," + str(total_cpu_usage) + "\n")
             f.close()
         except:
-            return 0
+            print("ERROR! CAN'T READ PROCESS CPU USAGE")
 
 def join_process_cpu_usage(powerlog_filename, process_filename, cpu_sockets):
     """Merges the three csv files on a pandas dataframe."""
 
-    powerlog_data = utils.read_powerlog_file(powerlog_filename, cpu_sockets)
+    [powerlog_data,general_data] = utils.read_powerlog_file(powerlog_filename, cpu_sockets)
     process_data = utils.read_csv_file(process_filename)
     cpu_time_in_microseconds = convert.array_to_microseconds(process_data['Time'],convert.time_to_microsecs)
     cpu_df = pd.DataFrame([], columns =['Time', 'Process CPU Usage(%)', 'Total CPU Usage(%)'])
@@ -65,7 +82,9 @@ def join_process_cpu_usage(powerlog_filename, process_filename, cpu_sockets):
 def main():
     [command,powerlog_filename,process_filename,nvidia_smi_filename,total_process_data,interval,cpu_sockets] = configuration.get_configuration()
 
-    if(False):
+    measure_baseline_wattage()
+
+    if(True):
         configuration.initialize_files(powerlog_filename, process_filename, nvidia_smi_filename)
 
         thread_cpu = Process(target = measure_process_cpu_usage, args = (process_filename, 0, ))
