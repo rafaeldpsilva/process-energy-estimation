64f878c3db2d9ef5e9c10c9fa6085097a9ab6c94
Rafael Silva <1190979@isep.ipp.pt>
2022-08-29 14:51:20 +0100

    gpu base report

--- process-energy-estimation/report.py
+++ process-energy-estimation/report.py
@@ -76,7 +76,10 @@ def print_base_results(elapsed_time,cpu,dram):
     total_consumption = total_average_cpu_power + total_average_gpu_power + average_dram_power
     print("Baseline consumption: {} Watts".format(round(total_consumption,4)))
     total_consumption = (total_average_cpu_power + total_average_gpu_power) * elapsed_time + dram_energy
-    print("Baseline consumption: {} Wh\n\n".format(round(total_consumption/3600,4)))   
+    print("Baseline consumption: {} Wh\n\n".format(round(total_consumption/3600,4)))
+
+    reports_path = configuration.get_reports_path()
+    gpu_df.to_csv(reports_path+"/show_base_gpu.csv")
 
 def print_results(process_df,nvidia_smi_filename,cpu_sockets):
     print("\n\nProcess Consumption")
